5b6bb3e1500b8ddb47de1bf9308177d8e874f99e
lucas <lucas@DESKTOP-59GA8N0>
2020-05-25 15:59:53 -0300

    Avanço no projeto projeto atleta

---
+++ PojetoAtelta/Atleta.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PojetoAtelta
+{
+    class Atleta
+    {
+        private String nome;
+        private double peso;
+        private double altura;
+    }
+}
---
+++ PojetoAtelta/FrmAtleta.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PojetoAtelta
+{
+    public partial class FrmAtleta : Form
+    {
+        public FrmAtleta()
+        {
+            InitializeComponent();
+        }
+    }
+}
