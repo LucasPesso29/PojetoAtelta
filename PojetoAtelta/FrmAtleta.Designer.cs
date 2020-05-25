faee5a96abd12150b190c688f65ea27b63631e08
lucas <lucas@DESKTOP-59GA8N0>
2020-05-25 16:23:48 -0300

    Avanço projeto atleta 1

--- PojetoAtelta/FrmAtleta.Designer.cs
+++ PojetoAtelta/FrmAtleta.Designer.cs
@@ -28,33 +28,104 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.lbl_nome = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lbl_peso = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.lbl_altura = new System.Windows.Forms.Label();
+            this.lbl_idade = new System.Windows.Forms.Label();
+            this.btn_Calcular = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // label1
+            // lbl_nome
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(370, 38);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.lbl_nome.AutoSize = true;
+            this.lbl_nome.Location = new System.Drawing.Point(132, 71);
+            this.lbl_nome.Name = "lbl_nome";
+            this.lbl_nome.Size = new System.Drawing.Size(35, 13);
+            this.lbl_nome.TabIndex = 0;
+            this.lbl_nome.Text = "Nome";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(336, 54);
+            this.textBox1.Location = new System.Drawing.Point(173, 68);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 1;
+            // 
+            // lbl_peso
+            // 
+            this.lbl_peso.AutoSize = true;
+            this.lbl_peso.Location = new System.Drawing.Point(313, 71);
+            this.lbl_peso.Name = "lbl_peso";
+            this.lbl_peso.Size = new System.Drawing.Size(31, 13);
+            this.lbl_peso.TabIndex = 2;
+            this.lbl_peso.Text = "Peso";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(353, 68);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(172, 124);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 4;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(353, 124);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 5;
+            // 
+            // lbl_altura
+            // 
+            this.lbl_altura.AutoSize = true;
+            this.lbl_altura.Location = new System.Drawing.Point(132, 127);
+            this.lbl_altura.Name = "lbl_altura";
+            this.lbl_altura.Size = new System.Drawing.Size(34, 13);
+            this.lbl_altura.TabIndex = 6;
+            this.lbl_altura.Text = "Altura";
+            // 
+            // lbl_idade
+            // 
+            this.lbl_idade.AutoSize = true;
+            this.lbl_idade.Location = new System.Drawing.Point(313, 127);
+            this.lbl_idade.Name = "lbl_idade";
+            this.lbl_idade.Size = new System.Drawing.Size(34, 13);
+            this.lbl_idade.TabIndex = 7;
+            this.lbl_idade.Text = "Idade";
+            // 
+            // btn_Calcular
+            // 
+            this.btn_Calcular.Location = new System.Drawing.Point(247, 178);
+            this.btn_Calcular.Name = "btn_Calcular";
+            this.btn_Calcular.Size = new System.Drawing.Size(109, 35);
+            this.btn_Calcular.TabIndex = 8;
+            this.btn_Calcular.Text = "Calcular";
+            this.btn_Calcular.UseVisualStyleBackColor = true;
             // 
             // FrmAtleta
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(618, 315);
+            this.Controls.Add(this.btn_Calcular);
+            this.Controls.Add(this.lbl_idade);
+            this.Controls.Add(this.lbl_altura);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.lbl_peso);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbl_nome);
             this.Name = "FrmAtleta";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -64,8 +135,15 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl_nome;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lbl_peso;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label lbl_altura;
+        private System.Windows.Forms.Label lbl_idade;
+        private System.Windows.Forms.Button btn_Calcular;
     }
 }
 
